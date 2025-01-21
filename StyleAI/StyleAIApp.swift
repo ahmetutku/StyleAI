1317c04947a0adf38226d1ea0747accbf7a023b1
ahmetutku <ahamamcioglu2014@gmail.com>
2025-01-20 21:29:12 -0500

    Initial Commit

---
+++ StyleAI/StyleAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StyleAIApp.swift
+//  StyleAI
+//
+//  Created by Ahmet Hamamcioglu on 20.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct StyleAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StyleAITests/StyleAITests.swift
@@ -0,0 +1,17 @@
+//
+//  StyleAITests.swift
+//  StyleAITests
+//
+//  Created by Ahmet Hamamcioglu on 20.01.2025.
+//
+
+import Testing
+@testable import StyleAI
+
+struct StyleAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
