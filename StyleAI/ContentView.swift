c48fc19c111c2220a6db792883332090015367be
ahmetutku <ahamamcioglu2014@gmail.com>
2025-01-20 22:27:09 -0500

    Trial with photopicker, allowing user to pick their own photos

--- StyleAI/ContentView.swift
+++ StyleAI/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import PhotosUI
+
 
 struct ContentView: View {
+    @State private var avatarItem: PhotosPickerItem?
+    @State private var avatarImage: Image?
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, welcome to StyleAI!")
+            PhotosPicker("Select avatar", selection: $avatarItem, matching: .images)
+
+                        avatarImage?
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 300, height: 300)
         }
         .padding()
+        .onChange(of: avatarItem) {
+            Task {
+                if let loaded = try? await avatarItem?.loadTransferable(type: Image.self) {
+                    avatarImage = loaded
+                } else {
+                    print("Failed")
+                }
+            }
+        }
     }
 }
 
