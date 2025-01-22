27e2f0e462d8145ef61707b6ceca886253503f8f
ahmetutku <ahamamcioglu2014@gmail.com>
2025-01-21 20:43:08 -0500

    got multiple images to get displayed

--- StyleAI/ContentView.swift
+++ StyleAI/ContentView.swift
@@ -8,26 +8,62 @@
 import SwiftUI
 import PhotosUI
 
-
 struct ContentView: View {
     @State private var avatarItem: PhotosPickerItem?
     @State private var avatarImage: Image?
+    @State var selectedItems: [PhotosPickerItem] = []
+    @State var images: [UIImage] = []
     
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, welcome to StyleAI!")
+            
             PhotosPicker("Select avatar", selection: $avatarItem, matching: .images)
-
-                        avatarImage?
+            avatarImage?
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+            
+            PhotosPicker(selection: $selectedItems, matching: .images) {
+                Text("Select Multiple Photos")
+            }
+            .onChange(of: selectedItems) { selectedItems in
+                images = []
+                for item in selectedItems {
+                    item.loadTransferable(type: Data.self) { result in
+                        switch result {
+                        case .success(let imageData):
+                            if let imageData, let uiImage = UIImage(data: imageData) {
+                                DispatchQueue.main.async {
+                                    self.images.append(uiImage)
+                                }
+                            } else {
+                                print("No supported content type found.")
+                            }
+                        case .failure(let error):
+                            print(error)
+                        }
+                    }
+                }
+            }
+            
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(images, id: \.self) { image in
+                        Image(uiImage: image)
                             .resizable()
                             .scaledToFit()
-                            .frame(width: 300, height: 300)
+                            .frame(width: 100, height: 100)
+                            .cornerRadius(8)
+                    }
+                }
+            }
         }
         .padding()
-        .onChange(of: avatarItem) {
+        .onChange(of: avatarItem) { _ in
             Task {
                 if let loaded = try? await avatarItem?.loadTransferable(type: Image.self) {
                     avatarImage = loaded
@@ -39,6 +75,8 @@ struct ContentView: View {
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
