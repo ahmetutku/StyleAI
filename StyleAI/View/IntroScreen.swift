c618cb4e6ddf535559abeae47f92acbadeecb30d
ahmetutku <ahamamcioglu2014@gmail.com>
2025-01-24 20:32:46 -0500

    Changed styling for the intro screen

--- StyleAI/View/IntroScreen.swift
+++ StyleAI/View/IntroScreen.swift
@@ -15,17 +15,24 @@ struct IntroScreen: View {
         VStack {
             Text("Welcome to StyleAI")
                 .font(.largeTitle)
-                .padding()
+                .fontWeight(.bold)
+                .foregroundColor(.accentColor)
+                .padding(.top)
             
             Text("StyleAI helps you dress better by learning your preferences and using ML and the information you provide to make your outfits the best outfits for you")
+                .padding(.top)
+                .font(.title2)
+                .fontWeight(.bold)
                 .multilineTextAlignment(.center)
-                .padding()
+//                .foregroundColor(Color(.subtitle))
             
             Button(action: {
                 showIntro = false // Change state to show the main screen
             }) {
                 Text("Let's Get It")
                     .padding()
+                    .font(.title2)
+                    .fontWeight(.bold)
                     .frame(maxWidth: .infinity)
                     .background(Color.blue)
                     .foregroundColor(.white)
