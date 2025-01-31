7717d5eb8e035e64cba3e413029870b86144aa5e
ahmetutku <ahamamcioglu2014@gmail.com>
2025-01-31 17:52:39 -0500

    embded closet view in a vstack

--- StyleAI/View/ClosetView.swift
+++ StyleAI/View/ClosetView.swift
@@ -10,24 +10,23 @@ struct ClosetView: View {
     var body: some View {
         ZStack{
             Color("background_color")
-            Spacer()
-            Text("My Closet").font(.title2)
-                .fontWeight(.bold).foregroundColor(.accentColor)
-            Spacer()
-            Button(action: {
-            }) {
-                Text("Add a Piece")
-                    .padding()
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity)
-                    .background(Color(.accent))
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+            VStack{
+                Text("My Closet").font(.title2)
+                    .fontWeight(.bold).foregroundColor(.accentColor)
+                Button(action: {
+                }) {
+                    Text("Add a Piece")
+                        .padding()
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .background(Color(.accent))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .foregroundColor(.accentColor)
             }
-            .padding(.top, 100.0)
-            .foregroundColor(.accentColor)        }
-        .background(Color("background_color").ignoresSafeArea())
+
+        }.background(Color("background_color").ignoresSafeArea())
     }
 }
 
