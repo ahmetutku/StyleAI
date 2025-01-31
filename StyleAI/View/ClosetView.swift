32d4c9afa4bed061ef2d8b428324c570b3346c7e
ahmetutku <ahamamcioglu2014@gmail.com>
2025-01-30 19:50:07 -0500

    created new file for the closet view

---
+++ StyleAI/View/ClosetView.swift
@@ -0,0 +1,13 @@
+//
+//  ClosetView.swift
+//  StyleAI
+//
+//  Created by Ahmet Hamamcioglu on 30.01.2025.
+//
+import SwiftUI
+
+struct ClosetView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
