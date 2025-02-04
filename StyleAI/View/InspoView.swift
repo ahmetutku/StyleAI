89d8b9b5d8b8500c888cf0507a450a11250f49e7
ahmetutku <ahamamcioglu2014@gmail.com>
2025-02-03 20:15:42 -0500

    Added inspo view for the dropdown menu to view

---
+++ StyleAI/View/InspoView.swift
@@ -0,0 +1,19 @@
+//
+//  InspoView.swift
+//  StyleAI
+//
+//  Created by Ahmet Hamamcioglu on 3.02.2025.
+//
+
+import SwiftUI
+
+struct InspoView: View {
+    var body: some View {
+        ZStack {
+            Color("background_color").ignoresSafeArea()
+            
+
+        }
+
+    }
+}
