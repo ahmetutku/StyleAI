e2c97621ca0421ad3d011b7c4943f53e5400578b
ahmetutku <ahamamcioglu2014@gmail.com>
2025-02-06 17:43:26 -0500

    created new file contianing closet image full screen view

---
+++ StyleAI/View/ClosetImageView.swift
@@ -0,0 +1,53 @@
+//
+//  ClosetImageView.swift
+//  StyleAI
+//
+//  Created by Ahmet Hamamcioglu on 6.02.2025.
+//
+import SwiftUI
+
+struct ClosetImageView: View {
+    let image: UIImage
+    let removeBackground: (UIImage) -> Void
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var isProcessing = false
+
+    var body: some View {
+        ZStack {
+            Color.black.ignoresSafeArea()
+            VStack {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(10)
+                    .shadow(radius: 5)
+
+                Button(action: {
+                    isProcessing = true
+                    removeBackground(image)
+                    isProcessing = false
+                }) {
+                    if isProcessing {
+                        ProgressView()
+                    } else {
+                        Text("Remove Background")
+                            .padding()
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                }
+                .padding()
+
+                Button("Close") {
+                    presentationMode.wrappedValue.dismiss()
+                }
+                .padding()
+                .foregroundColor(.white)
+            }
+        }
+    }
+}
