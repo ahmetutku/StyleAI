a32fc8f455465dd9e13fb515e42f74f1f3232697
ahmetutku <ahamamcioglu2014@gmail.com>
2025-02-12 18:08:02 -0500

    Added new mdoel closet item

---
+++ StyleAI/Models/ClosetItemImage.swift
@@ -0,0 +1,14 @@
+//
+//  ClosetItemImage.swift
+//  StyleAI
+//
+//  Created by Ahmet Hamamcioglu on 12.02.2025.
+//
+
+
+import SwiftUI
+
+struct ClosetItemImage: Identifiable {
+    let id = UUID()
+    let name: String
+}
