3d6d73900cf634aeb9e3d2626b366c5bab466832
ahmetutku <ahamamcioglu2014@gmail.com>
2025-02-16 15:53:43 -0500

    modified fullscreenclosetimageview to accept ui image

--- StyleAI/View/FullScreenClosetImageView.swift
+++ StyleAI/View/FullScreenClosetImageView.swift
@@ -9,14 +9,13 @@
 import SwiftUI
 
 struct FullScreenClosetImageView: View {
-    let imageName: String
+    let image: UIImage
     @Environment(\.dismiss) var dismiss
 
     var body: some View {
         ZStack {
             Color("background_color")
-            Image(imageName)
-                .resizable()
+            Image(uiImage: image).resizable()
                 .scaledToFit()
                 .onTapGesture {
                     dismiss()
